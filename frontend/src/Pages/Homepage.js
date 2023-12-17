ea34270f4d58476bd4b1919882cb2d619b8d74a0
aakash gadekar <Aakash Gadekar>
2023-12-17 09:50:20 +0530

    forth commit

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Container,Box,Text } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
+
 
 const Homepage=()=>{
-    return <div>Home</div>
+    return (
+    <Container maxW='xl' centerContent>
+          <Box
+          d="flex"
+          justifyContent="center"
+          p={3}
+          bg={"white"}
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="1g"
+          borderWidth="1px"
+          >
+              <Text 
+              fontSize="4xl"
+               fontFamily="Work sans"
+               color="black" centerContent>Aakash Chatbot</Text>
+          </Box>
+          <Box bg="white" w="100%" p={4} borderRadius="1g" 
+          borderWidth="1px" color="black"> 
+          <Tabs variant='soft-rounded' colorScheme='green'>
+                <TabList mb="1em">
+                    <Tab width="50%">Login</Tab>
+                    <Tab width="50%">Sign Up</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                    <Login/>
+                    </TabPanel>
+                    <TabPanel>
+                   <Signup/>
+                    </TabPanel>
+                </TabPanels>
+                </Tabs>
+
+          </Box>
+    </Container>
+    );
 };
 
 export default Homepage;
